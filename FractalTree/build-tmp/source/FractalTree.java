ceb3e55429682f0591fe5ff48f7ed782d3d6f24f
Marvin Lee <marvinlee.github@gmail.com>
2019-08-28 22:01:54 -0700

    Finished it by reflecting over the y axis at x=width/2 instead of making weird tree. Recursive hell

--- FractalTree/build-tmp/source/FractalTree.java
+++ FractalTree/build-tmp/source/FractalTree.java
@@ -1,25 +1,41 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class FractalTree extends PApplet {
+
 float startSize=300;
 float sizeLimit=1;
 int counter=1;
-float wideScale=9.0/10;
+float wideScale=9.0f/10;
 int startAngle=0;
 int angleChange=15;
 int startWideness=200;
-void setup()
+public void setup()
 {
-	size(750,750);
+	
 	frameRate(1);
 	background(200,200,200);
 	noLoop();
 }
-void draw()
+public void draw()
 {
 	fill(200,200,200);
 	rect(0, 0, width, height);
 	sticks(width/2,height,startWideness,0,counter,true);
 	counter++;
 }
-void sticks(float x,float y,float wideness,float angle,int count, boolean positive)
+public void sticks(float x,float y,float wideness,float angle,int count, boolean positive)
 {
 	if(wideness<=sizeLimit)
 	{
@@ -41,11 +57,11 @@ void sticks(float x,float y,float wideness,float angle,int count, boolean positi
 		sticks(x-xDif,y-yDif,wideness*wideScale,angle+angleChange,count, false);
 	}
 }
-void mouseClicked()
+public void mouseClicked()
 {
 	redraw();
 }
-void keyPressed()
+public void keyPressed()
 {
 	if(key==' ')
 	{
@@ -83,3 +99,13 @@ void keyPressed()
 // 		sticks(x-wide*cos(angle2)+tall*cos(angle1),y-wide*sin(angle2)-tall*sin(angle1),wide/2,angle+wider,count);
 // 	}
 // }
+  public void settings() { 	size(750,750); }
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "FractalTree" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
